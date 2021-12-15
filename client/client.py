12ea0945e3d75523439ea83f4e657e0b0bc57e5b
SwJOKER <63502467+SwJOKER@users.noreply.github.com>
2021-12-15 12:23:56 +0300

    + ref

--- client/client.py
+++ client/client.py
@@ -1,9 +1,20 @@
+import logging
+
 import requests
 
+ID = 'Minion'
+MSG = 'Minion\'s message'
+
+def client_connect():
+   try:
+      name = ID
+      first_request = requests.get(f'http://127.0.0.1:8000/{name}')
+      md5 = first_request.text
+      message = MSG
+      second_request = requests.get(f'http://127.0.0.1:8001/{md5}/{name}/{message}')
+      print(second_request.text)
+   except:
+      logging.error('Something went wrong')
+
 if __name__ == '__main__':
-   name = 'SwJOKER1'
-   first_request = requests.get(f'http://127.0.0.1:8000/{name}')
-   md5 = first_request.text
-   message = input()
-   second_request = requests.get(f'http://127.0.0.1:8001/{md5}/{name}/{message}')
-   print(second_request.text)
+   client_connect()
